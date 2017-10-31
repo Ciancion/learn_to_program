f50137dae633250e02e4577f5875e8efc78a3226
Valentina <valentina.rmo@gmail.com>
2017-10-31 11:55:58 +0000

    Excercise 5.6 cap5

---
+++ cap5/full_name_greeting.rb
@@ -0,0 +1,7 @@
+puts "What\'s your name?"
+name = gets.chomp
+puts "What\'s your middle name?"
+middle_name = gets.chomp
+puts "What\'s your last name?"
+last_name = gets.chomp
+puts "Nice to meet you #{name} #{middle_name} #{last_name}"
