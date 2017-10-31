b7c8d18ee91e5a0a2d0cd1ff59b0a71c9cfd15b4
Valentina <valentina.rmo@gmail.com>
2017-10-31 20:23:22 +0000

    Looping exercise 2

---
+++ cap7/leap_years.rb
@@ -0,0 +1,13 @@
+puts "Insert a starting year "
+start_year = gets.chomp.to_i
+puts "Insert an ending year"
+end_year = gets.chomp.to_i
+
+while start_year != (end_year +1)
+
+  if start_year % 4 == 0
+    puts start_year
+  else
+  end
+start_year = start_year +1
+end
